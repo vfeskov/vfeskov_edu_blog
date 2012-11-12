bb12d30b8e965e22badc025052f1fce280ac660c
Vladimir Feskov <v@vladimirfeskov.com>
2012-11-12 19:13:37 +0200

    contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,24 +5,24 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => 'Testing out stuff')
     end
 
     it "should have h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
   end
 
   describe "Help page" do
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => '| Help')
     end
 
     it "should have h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
@@ -31,12 +31,12 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', :text => '| About Us')
     end
 
     it "should have h1 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should  have_selector('h1', :text => 'About Us')
     end
   end
@@ -44,12 +44,12 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', text: 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title',
                                 text: "Testing out stuff | Contact")
     end
