18312b53144acaff9e983e0b8fa15e8adb8d2493
Vladimir Feskov <v@vladimirfeskov.com>
2012-11-16 16:47:40 +0200

    added index action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
+  end
   def show
     @user = User.find(params[:id])
   end
