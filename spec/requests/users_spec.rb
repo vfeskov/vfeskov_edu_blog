aba366bb35b2817d6f15707bacde16b23e5b2a17
Vladimir Feskov <v@vladimirfeskov.com>
2012-11-16 16:39:54 +0200

    Finish user signup

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,11 +4,32 @@ describe "Users" do
 
   subject { page }
 
-  describe "Sign up page" do
+  describe "sign up page" do
+
+    let(:submit) { "Create my account" }
+
     before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
+
+    describe "when all fields are blank" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "when all fields are specified and valid" do
+      before {
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "a@a.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      }
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
   end
 
   describe "profile page" do
@@ -19,4 +40,5 @@ describe "Users" do
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
+
 end
